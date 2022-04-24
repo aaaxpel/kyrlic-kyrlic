f6cb2894ce40502c05a9f4ec7412872e665f0603
aaaxpel <sqlc@mail.ru>
2022-04-24 17:16:32 +0700

    Перевод на русский + изменение некоторых компонентов, добавление новых

---
+++ views/Admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Админская страничка с редактированием и удалением</h1>
+  <router-link to="/">Вернуться назад</router-link>
+  <!-- Через v-for можно будет сделать много их -->
+  <Ticket />
+</template>
+
+<script>
+import Ticket from './components/Ticket'
+
+export default {
+  name: "AdminPage",
+  components: {
+    Ticket
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ views/components/Ticket.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Билет №</h1>
+  <form>
+    <input type="text" name="from-location" placeholder="Откуда-место">
+    <input type="date" name="from-date" placeholder="Откуда-дата">
+
+    <input type="text" name="to-location" placeholder="Куда-место">
+    <input type="date" name="to-date" placeholder="Куда-время">
+
+    <input type="number" name="price" placeholder="Стоимость">
+
+    <!-- Очень временные кнопки -->
+    <button @click="redactTicket">Редактировать</button>
+    <button @click="deleteTicket">Удалить</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "TicketComponent",
+  methods: {
+    redactTicket() {
+      console.log('Редактирование')
+    },
+    deleteTicket() {
+      console.log('Удаление')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
